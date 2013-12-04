6afb6481542eaa8dc76c8b9ff8fe2d88d22155dc
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-04 01:56:04 -0500

    Parse out MAC address and timestamp in ysniff.py

--- ysniff.py
+++ ysniff.py
@@ -2,8 +2,22 @@
 
 import fileinput
 
-index = 12
+mac_index = 12
+time_index = 1
+MAC_LEN = 17
+SAMPLE_PERIOD = 300 # Seconds. 5 minutes.
+maclist = []
+buffer = {}
+# TODO: add maclists to buffer every sample period.
+# TODO: Upload buffer to AWS every collection period.
 for line in fileinput.input():
     splitline = line.split(" ")
-    if index < len(splitline):
-        print line.split(" ")[12]
+    if mac_index < len(splitline):
+        mac = splitline[mac_index]
+        if mac == "DA:Broadcast":
+            mac = splitline[mac_index+1]
+        ts = splitline[time_index]
+        mac = mac[len(mac)-MAC_LEN:]
+        if mac not in maclist:
+            maclist.append(mac)
+        print ts,mac
