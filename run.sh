79e4acd0e4c35b7eef5ef097fd6d6c952efeb8e8
Sam Anklesaria <amsay@amsay.net>
2013-12-05 00:29:25 -0500

    joining YaleGuest on run

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 # TODO: Click the I Agree button on YaleGuest
 # curl --data "email=YaleGuest@yale.edu&cmd=cmd" http://10.160.252.249/auth/index.html/u
 
+ifconfig wlan0 up
+iwconfig wlan0 essid YaleGuest
 sudo airmon-ng start wlan0
 sudo tcpdump -e -i mon0 | /home/pi/ysniff-software/ysniff.py
