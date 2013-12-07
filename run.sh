d2639191ef9af80eeac78dc7ef260822b78be3e6
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-07 16:08:20 -0500

    Add environment variables for running ysniff to install.sh and run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # TODO: Click the I Agree button on YaleGuest
 # curl --data "email=YaleGuest@yale.edu&cmd=cmd" http://10.160.252.249/auth/index.html/u
 
+source /home/pi/.bashrc
 ifconfig wlan0 up
 iwconfig wlan0 essid YaleGuest
 sudo airmon-ng start wlan0
