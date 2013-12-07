83799d81fe734938024aa1f5d4a438fba3cc1807
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-07 16:00:15 -0500

    Add uploading to AWS machinery

--- ysniff.py
+++ ysniff.py
@@ -3,6 +3,7 @@
 import boto.rds
 import fileinput
 import sys
+import os
 
 mac_index = 12
 time_index = 1
@@ -14,8 +15,8 @@ PUSH_TO_AWS_PERIOD = 3600 # Seconds. One hour.
 maclist = set()
 buffer = {}
 
-conn = boto.rds.connect_to_region("us-west-2",aws_access_key_id=sys.argv[1],aws_secret_key_id=sys.argv[2])
-    aws_secret_access_key='<aws secret key>')
+conn=boto.connect_sdb()
+domain=conn.get_domain('tmp_ysniff')
 
 # TODO: Upload buffer to AWS every collection period.
 for line in fileinput.input():
@@ -26,27 +27,31 @@ for line in fileinput.input():
             mac = splitline[mac_index+1]
         ts = int(splitline[time_index][:-2])
         mac = mac[len(mac)-MAC_LEN:]
-        maclist.update(mac)
 
-        # move maclist to buffer every SAMPLE_PERIOD
-        if start_t_us is 0:
-            start_t_us = ts
-        elif ts - start_t_us > (SAMPLE_PERIOD * 1000000):
-            buffer[start_t_us] = maclist
-            maclist = set()
+        # Make list of timestamps for each mac
+        if mac not in buffer:
+            buffer[mac]=[]
+
+        # Only pair timestamp to mac address once
+        if start_t_us not in buffer[mac]:
+            buffer[mac].append(start_t_us)
+
+        # Update start_t_us every SAMPLE_PERIOD
+        if start_t_us is 0 or ts - start_t_us > (SAMPLE_PERIOD * 1000000):
             start_t_us = ts
 
-        # upload buffer every PUSH_TO_AWS_PERIOD
+        # upload buffer to AWS every PUSH_TO_AWS_PERIOD
         if start_u_us is 0:
             start_u_us = ts
         elif ts - start_u_us > (PUSH_TO_AWS_PERIOD  * 1000000):
-            # upload buffer here
+            for key in buffer:
+                item = domain.get_item(key)
+                for timestamp in buffer[key]:
+                    item[timestamp] = os.environ['PI_LOCATION']
+                item.save()
+
             buffer = {}
             start_t_us = ts
-
-
-
-        print ts,mac
 
 #print buffer, len(buffer)
 
