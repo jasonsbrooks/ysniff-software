0e250a9bcf203ecac6c9a21865bb5ca599f29247
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-07 23:13:32 -0500

    Update run instructions

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@
 source /home/pi/.bashrc
 ifconfig wlan0 up
 iwconfig wlan0 essid YaleGuest
+sleep 10
+iwconfig wlan0 essid YaleGuest
+sleep 10
 curl --data "email=YaleGuest@yale.edu&cmd=cmd" http://10.160.252.249/auth/index.html/u
 sudo airmon-ng start wlan0
 sudo tcpdump -e -i mon0 | /home/pi/ysniff-software/ysniff.py
