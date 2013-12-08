1ae000fd9d63fa0fbda4f2eea376223c256785b4
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-07 23:07:20 -0500

    Automate connection to YaleGuest

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # TODO: Click the I Agree button on YaleGuest
-# curl --data "email=YaleGuest@yale.edu&cmd=cmd" http://10.160.252.249/auth/index.html/u
 
 source /home/pi/.bashrc
 ifconfig wlan0 up
 iwconfig wlan0 essid YaleGuest
+curl --data "email=YaleGuest@yale.edu&cmd=cmd" http://10.160.252.249/auth/index.html/u
 sudo airmon-ng start wlan0
 sudo tcpdump -e -i mon0 | /home/pi/ysniff-software/ysniff.py
