da8d08e23367d1b5d3c11ba48986d3669fd4e4c7
Alejandro Carrillo <aecarrillo594@gmail.com>
2013-12-09 20:56:15 -0500

    Fix IP address getting

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ git pull # Fetch updates
 
 export LC_ALL=C
 
-eth_ip = $(ifconfig | grep ether | head -1 | cut -c8-24)
-time = $(date +%s)
+export eth_ip = $(ifconfig | grep inet | head -1 | cut -c21-34)
+export time = $(date +%s)
 
 export /home/pi/.bashrc
 /home/pi/ysniff-software/tools/simpledb put dev-pi-locations $PI_LOCATION IP=$eth_ip LAST_PUSH=$time
